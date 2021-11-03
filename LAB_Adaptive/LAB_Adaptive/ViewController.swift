0d13fb2957381a7fd88aba57d3f96903a084b585
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-11-03 11:31:42 +0300

    Done HW

---
+++ LAB_Adaptive/LAB_Adaptive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB_Adaptive
+//
+//  Created by Munira abdullah on 28/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
