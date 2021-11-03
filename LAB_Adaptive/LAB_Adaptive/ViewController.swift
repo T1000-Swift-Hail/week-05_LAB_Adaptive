8f79ca34e2cc66df82a8c3345a64f940fb845cdd
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-11-03 11:36:46 +0300

    done -week-05_LAB_Adaptive

---
+++ LAB_Adaptive/LAB_Adaptive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB_Adaptive
+//
+//  Created by Hesah Alqhtani on 03/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
