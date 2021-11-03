f01c34c5ef2e731a9b8a986156ad20b70390b46c
Monafh <name@gmail.com>
2021-11-03 15:07:04 +0300

    OK

---
+++ Adaptive/Adaptive/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Adaptive
+//
+//  Created by Monafh on 28/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITextFieldDelegate {
+    
+//    let textFiled = UITextField()
+//     
+//     textFiled.placeholder = "User Name "
+//     textFiled.translatesAutoresizingMaskIntoConstraints = false
+//     textFiled.borderStyle = .line
+//    
+//     
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
