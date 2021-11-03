553ff34c71fceca301db4838aade9fef5b46c127
monaaleid <92249905+monaaleid@users.noreply.github.com>
2021-11-03 12:22:01 +0300

    Done

---
+++ week-05_LAB_Adaptive/week-05_LAB_Adaptive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-05_LAB_Adaptive
+//
+//  Created by mona aleid on 28/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
