c776d4338ca15151ba207dace145245bf1d67d32
dalal92 <dalal@Dalals-MacbookPro.local>
2021-11-03 22:13:02 +0300

    some improvments^^

--- AdaptiveInterfaces/AdaptiveInterfaces/ViewController.swift
+++ AdaptiveInterfaces/AdaptiveInterfaces/ViewController.swift
@@ -14,15 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        stackViews.backgroundColor = nil
+        
         if self.traitCollection.userInterfaceStyle == .dark {
             
-            stackViews.backgroundColor = UIColor(displayP3Red: 0.194, green: 0.264, blue: 0.386, alpha: 0.85)
-            stackViews.tintColor = UIColor(displayP3Red: 0.432, green:  0.923, blue: 0.923, alpha: 0.85)
+            stackViews.backgroundColor = UIColor(displayP3Red: 0.194, green: 0.264, blue: 0.386, alpha: 0.0)
+            stackViews.tintColor = UIColor(displayP3Red: 0.432, green:  0.923, blue: 0.923, alpha: 1)
             
         } else {
             
-            stackViews.backgroundColor = UIColor(displayP3Red: 0.847, green: 0.847, blue: 0.847, alpha: 0.85)
-            stackViews.tintColor = UIColor(displayP3Red: 0.280, green:  0.223, blue: 1.000, alpha: 0.85)
+            stackViews.backgroundColor = UIColor(displayP3Red: 0.847, green: 0.847, blue: 0.847, alpha: 0.0)
+            stackViews.tintColor = UIColor(displayP3Red: 0.280, green:  0.223, blue: 1.000, alpha: 1)
             
         }
         
